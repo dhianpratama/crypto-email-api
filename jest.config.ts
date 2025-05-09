9f763ef55ab62264dce855d6e97d82cf2b6c2d0e
Dhian <muthidp@gmail.com>
2025-05-09 11:19:39 +1000

    Update unit test

--- jest.config.ts
+++ jest.config.ts
@@ -4,6 +4,9 @@ const config: Config = {
   testEnvironment: 'node',
   preset: 'ts-jest',
   roots: ['<rootDir>'],
+  moduleNameMapper: {
+    '^@shared/(.*)$': '<rootDir>/shared/$1',
+  },
 };
 
 export default config;
